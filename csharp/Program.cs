8608f94ea4e82829c574d690733083d156354d4e
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2024-03-21 20:34:33 -0400

    Add Queen's Attack 2

---
+++ csharp/Program.cs
@@ -0,0 +1,2 @@
+﻿// Queen's Attack 2
+Console.WriteLine(QueensAttack2.Run(100_000, 0, 4187, 5068, [[]]));
---
+++ csharp/QueensAttack2.cs
@@ -0,0 +1,42 @@
+public class QueensAttack2
+{
+    public static int Run(int n, int k, int r_q, int c_q, List<List<int>> obstacles)
+    {
+        // Build matrix
+        int[,] board = new int[n, n];
+        foreach(List<int> obstacle in obstacles)
+        {
+            board[obstacle[0] - 1, obstacle[1] - 1] = 1;
+        }
+
+        // Starting at the queen, walk until we encounter an obstacle or the board edge
+        (int, int) queenPosition = (r_q - 1, c_q - 1);
+        int up = GetFreeSpaces(board, queenPosition, (1, 0));
+        int upRight = GetFreeSpaces(board, queenPosition, (1, 1));
+        int right = GetFreeSpaces(board, queenPosition, (0, 1));
+        int downRight = GetFreeSpaces(board, queenPosition, (-1, 1));
+        int down = GetFreeSpaces(board, queenPosition, (-1, 0));
+        int downLeft = GetFreeSpaces(board, queenPosition, (-1, -1));
+        int left = GetFreeSpaces(board, queenPosition, (0, -1));
+        int upLeft = GetFreeSpaces(board, queenPosition, (1, -1));
+        return up + upRight + upLeft + right + downRight + down + downLeft + left + upLeft;
+    }
+
+    private static int GetFreeSpaces(int[,] board, (int, int) queenPosition, (int, int) step)
+    {
+        int freeSpaces = 0;
+        (int, int) currentPosition = (queenPosition.Item1 + step.Item1, queenPosition.Item2 + step.Item2);
+        while
+        (
+            currentPosition.Item1 < board.GetLength(0) &&
+            currentPosition.Item1 >= 0 &&
+            currentPosition.Item2 < board.GetLength(1) &&
+            currentPosition.Item2 >= 0 &&
+            board[currentPosition.Item1, currentPosition.Item2] == 0
+        ) {
+            freeSpaces++;
+            currentPosition = (currentPosition.Item1 + step.Item1, currentPosition.Item2 + step.Item2);
+        }
+        return freeSpaces;
+    }
+}
